3d698c9dcd777adb84ff1989a1a4026ab5ffdc9e
Jewar0 <joar.rinaldo.roos@gmail.com>
2022-02-14 16:56:35 +0100

    pushed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header"
 
 function App() {
   const name = 'Joar'
   return (
     <div className="container">
-      <h1>Hello from {name}</h1>
-      <h2>Hey </h2>
+      <Header />
     </div>
   );
 }
+
 
 export default App;
